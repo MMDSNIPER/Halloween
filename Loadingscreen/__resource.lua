289424765c26ad65f7ae223169bf9cabb628b8b2
Mamad SNIPER <Mamadsniper>
2019-10-29 03:32:36 +0330

    1.0.0

---
+++ Loadingscreen/__resource.lua
@@ -0,0 +1,34 @@
+--[[
+    Halloween Scripts
+    Copyright C 2019  SNIPER
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    at your option any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+]]
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+loadscreen 'index.html'
+description 'Halloween Script Pack'
+version '1.0.0'
+
+files {
+    'index.html',
+    'imgs/halloween.png',
+    'imgs/joghd3.png',
+    'imgs/logo_halloween2.png',
+    'imgs/snowfall3.png',
+    'stylesheet.css',
+    'config.js',
+    'app.js',
+    'config.css',
+    'audio1.mp3'
+}
---
+++ Loadingscreen/config.js
@@ -0,0 +1,17 @@
+/* Need Help? Join my discord @ discord.gg/yWddFpQ */
+var config = {
+    /* In milliseconds */
+    "transitionInterval": 999999999999,
+    "imgInterval": 8000,
+    "music": true,
+    /* Insert the Youtube Video ID below */
+    "videoID": "jo4-FhqkNwQ",
+    /* Music Volume (0%-100%) | Lower is Quieter, Higher is Louder */
+    "musicVolume": 40,
+    "text": {
+        "title": "Sunrise RP",
+        "link": "discord.gg/3t2dcGC",
+    },
+    /* You dont have to include the '/imgs/' dir AND ALSO REMEMBER TO ADD THESE IMGS IN YOUR __resource.lua*/ 
+    "images": ['halloween.png']
+}
